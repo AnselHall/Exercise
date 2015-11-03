a695c17cf40ebe04d469fb6a1a96d45c2d4c691b
AnselHall <1206313836@qq.com>
2015-11-03 18:43:48 +0800

    1

--- app/src/main/java/com/exercise/exercise/MainActivity.java
+++ app/src/main/java/com/exercise/exercise/MainActivity.java
@@ -3,19 +3,30 @@ package com.exercise.exercise;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
+    private TextInputLayout mTextInputLayout;
+    private TextInputLayout mTextInputLayout2;
+    private EditText mEditText;
+    private int flag;
+    private FloatingActionButton mFloatButton;
+    private CoordinatorLayout mCoordinatorLayout;
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
@@ -23,14 +34,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawlayout);
-        mNavigationView = (NavigationView) findViewById(R.id.navigationview);
+        initView();
+
+        mTextInputLayout.setHintAnimationEnabled(true);
+        mTextInputLayout.setHint("请输入用户名");
+        mTextInputLayout2.setHint("请输入密码");
+
+//        mFloatButton.setBackgroundColor(Color.BLACK);
+        mFloatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(mCoordinatorLayout,"hello",Snackbar.LENGTH_LONG).show();
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
         setupDrawerContent(mNavigationView);
@@ -46,6 +67,16 @@ public class MainActivity extends AppCompatActivity {
         });*/
     }
 
+    private void initView() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawlayout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigationview);
+        mCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        mTextInputLayout = (TextInputLayout) findViewById(R.id.til_input);
+        mTextInputLayout2 = (TextInputLayout) findViewById(R.id.til_input2);
+        mEditText = (EditText) findViewById(R.id.et_input);
+        mFloatButton = (FloatingActionButton) findViewById(R.id.floatingactionbutton);
+    }
+
     private void setupDrawerContent(final NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -56,12 +87,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.naigationmenu,menu);
         return true;
-    }
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
